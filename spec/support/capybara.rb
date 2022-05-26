21c643116da59aa97918217bce4c9c7d94dcfca7
ken1flan <ken1flan@gmail.com>
2022-05-27 08:54:29 +0900

    フルスクリーンを保存するメソッドの追加
    capybaraのsave_screenshotだと、
    chromedriverでフルサイズで保存できないので…

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -13,4 +13,18 @@ RSpec.configure do |config|
     Rails.application.routes.default_url_options[:host] = Capybara.current_session.server.host
     Rails.application.routes.default_url_options[:port] = Capybara.current_session.server.port
   end
+
+  def save_full_screenshot(path)
+    width  = Capybara.page.execute_script("return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);")
+    height = Capybara.page.execute_script("return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);")
+
+    window = Capybara.current_session.driver.browser.manage.window
+    size_org = window.size
+
+    window.resize_to(width+100, height+100)
+
+    page.save_screenshot path
+
+    window.resize_to(*size_org)
+  end
 end
